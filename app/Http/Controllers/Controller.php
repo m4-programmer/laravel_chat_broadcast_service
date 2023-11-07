e330a0b8385f3ca9d7314a83cc912035a26dabb9
Adolphus Miracle <miraboy13@gmail.com>
2023-11-07 23:06:36 +0100

    feat: added a helper method for api responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,11 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function success($data, string $message, int $status=200){
+        return response()->json(['status'=>$status,'message'=>$message,'data'=>$data],$status);
+    }
+
+    public function error($message, int $status=500){
+        return response()->json(['status'=>$status,'message'=>$message],$status);
+    }
 }
